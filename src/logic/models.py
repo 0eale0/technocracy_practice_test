eeba2e437ce63f3efb73edc9727661eadd7b6caf
eale <RUSLIKPVP@yandex.ru>
2022-06-10 19:40:08 +0300

    add restframework into apps, add models from diagram, migrations, add models into admin

--- src/logic/models.py
+++ src/logic/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"null": True, "blank": True}
+NOT_NULLABLE = {"null": False, "blank": False}
+
+
+class User(AbstractUser):
+    pass
+
+
+class Note(models.Model):
+    header = models.CharField(max_length=200, **NOT_NULLABLE)
+    text = models.CharField(max_length=5000, **NULLABLE)
+    date = models.DateTimeField()
+
+
+class UserNote(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE)
+    note = models.ForeignKey(Note,
+                             on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    header = models.CharField(max_length=100, **NOT_NULLABLE)
+
+
+class NoteCategory(models.Model):
+    note = models.ForeignKey(Note,
+                             on_delete=models.CASCADE)
+
+    category = models.ForeignKey(Category,
+                                 on_delete=models.PROTECT)
--- src/notes/settings.py
+++ src/notes/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party apps
+    'rest_framework',
+    # My apps
+    "logic"
 ]
 
 MIDDLEWARE = [
@@ -107,6 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'logic.user'
 
 
 # Internationalization
